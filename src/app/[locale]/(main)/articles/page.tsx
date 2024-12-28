ec7cb3615fa8f6c7779d0ccbbe6a97b27b923310
Fox <urboifox@gmail.com>
2024-12-28 17:33:42 +0200

    feat: add translations for articles

--- src/app/[locale]/(main)/articles/page.tsx
+++ src/app/[locale]/(main)/articles/page.tsx
@@ -1,5 +1,6 @@
 import ArticleCard from "@/components/cards/article-card";
 import Pagination from "@/components/ui/pagination";
+import { getTranslations } from "next-intl/server";
 import { SearchParams } from "next/dist/server/request/search-params";
 
 export default async function ArticlesPage({
@@ -7,13 +8,14 @@ export default async function ArticlesPage({
 }: {
     searchParams: Promise<SearchParams>;
 }) {
+    const t = await getTranslations();
     const { page } = await searchParams;
     const articles = Array(6).fill(null);
 
     return (
         <div className="min-h-screen container py-16 flex flex-col gap-16">
             <div className="flex flex-col gap-8">
-                <h2 className="text-3xl font-semibold">المقالات المنشورة حديثًا</h2>
+                <h2 className="text-3xl font-semibold">{t("articles.title")}</h2>
                 <div className="flex flex-col xl:flex-row gap-8">
                     <ArticleCard imageContainerClassName="xl:aspect-[2.4]" />
                     <div className="flex flex-col sm:flex-row xl:flex-col gap-8 flex-1">
@@ -30,7 +32,7 @@ export default async function ArticlesPage({
                 <ArticleCard className="xl:flex-row" imageContainerClassName="xl:aspect-[2.4]" />
             </div>
             <main className="flex flex-col gap-6">
-                <h2 className="text-3xl font-semibold">جميع المقالات</h2>
+                <h2 className="text-3xl font-semibold">{t("articles.allArticles")}</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                     {articles.map((_, idx) => {
                         return <ArticleCard key={idx} />;
--- src/components/cards/article-card.tsx
+++ src/components/cards/article-card.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { Link } from "@/i18n/routing";
 import images from "@/lib/images";
 import { cn } from "@/utils/cn";
+import { useLocale, useTranslations } from "next-intl";
 
 export default function ArticleCard({
     className,
@@ -13,6 +14,11 @@ export default function ArticleCard({
     className?: string;
     imageContainerClassName?: string;
 }) {
+    const t = useTranslations();
+    const locale = useLocale();
+
+    const tags = [t("temp.articleTag")];
+
     return (
         <article className={cn("flex-1 flex flex-col gap-6", className)}>
             <div className={cn("relative flex-1 w-full aspect-[1.6]", imageContainerClassName)}>
@@ -21,13 +27,14 @@ export default function ArticleCard({
             <div className="flex flex-col gap-6 flex-1 justify-center">
                 <div className="flex flex-col gap-3">
                     <div className="flex items-center gap-1 text-primary-main font-semibold text-sm">
-                        <p>محمد أشرف</p> • <time>{moment().locale("ar").format("YYYY/MM/DD")}</time>
+                        <p>{t("temp.articleAuthor")}</p> •{" "}
+                        <time>{moment().locale(locale).format("YYYY/MM/DD")}</time>
                     </div>
                     <Link href="/articles/1" className="hover:underline">
-                        <h3 className="font-semibold text-lg lg:text-2xl text-dark-400 flex items-center gap-1 w-full justify-between">
-                            تأثير التكنولوجيا على التشخيص الطبي الحديث
+                        <h3 className="font-semibold text-lg lg:text-2xl text-dark-400 flex items-start gap-1 w-full justify-between line-clamp-2">
+                            {t("temp.articleTitle")}
                             <Image
-                                className="scale-x-[-1]"
+                                className="rtl:scale-x-[-1] mt-1"
                                 src={icons.goto}
                                 alt="Go to"
                                 width={24}
@@ -35,13 +42,12 @@ export default function ArticleCard({
                             />
                         </h3>
                     </Link>
-                    <p className="text-dark-300 line-clamp-3">
-                        استكشاف كيف ساهمت التطورات التكنولوجية في تحسين دقة التشخيص الطبي، من الذكاء
-                        الاصطناعي إلى التحليل الجيني، ودورها في تحسين رعاية المرضى.
-                    </p>
+                    <p className="text-dark-300 line-clamp-3">{t("temp.articleDescription")}</p>
                 </div>
                 <div className="flex items-center gap-2">
-                    <Tag name="بحوثات" />
+                    {tags.map((tag, index) => (
+                        <Tag key={index} name={tag} />
+                    ))}
                 </div>
             </div>
         </article>

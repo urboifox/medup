61a4a0d683dd423466e36edab93aa9d713ac14f2
Fox <urboifox@gmail.com>
2025-02-18 18:39:53 +0200

    add logo

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -1,4 +1,5 @@
 import LanguageSwitcher from "@/components/layout/language-switcher";
+import Logo from "@/components/ui/logo";
 import { Link } from "@/i18n/routing";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
@@ -7,9 +8,7 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
             <div className="bg-white shadow-[11px_4px_15px_0_#0000001A]">
                 <header className="container py-6 flex items-center justify-between">
                     <Link href={"/"} className="flex items-center gap-2">
-                        <span className="text-2xl font-semibold text-[#18191c]">
-                            Med<span className="text-primary-main">Up</span>
-                        </span>
+                        <Logo />
                     </Link>
                     <LanguageSwitcher />
                 </header>
--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,7 +1,9 @@
+import images from "@/lib/images";
+import Image from "next/image";
+
 export default function Logo() {
-    return (
-        <span className="text-2xl font-semibold text-[#18191c]">
-            Med<span className="text-primary-main">Up</span>
-        </span>
-    );
+    return <Image src={images.logo} alt="Logo" width={150} height={50} />;
 }
+// {/* <span className="text-2xl font-semibold text-[#18191c]"> */}
+// {/*     Med<span className="text-primary-main">Up</span> */}
+// {/* </span> */}
